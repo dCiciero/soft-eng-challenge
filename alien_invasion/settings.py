e555b8456662f49afbb41abf64aacd6f0681650b
Ogaga A. Ivhurie <ivhurie01@yahoo.co.uk>
2022-04-28 21:33:23 +0100

    added some tests+

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -14,9 +14,9 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['localhost','127.0.0.1','flugelchallenge.herokuapp.com/','ogcicero.pythonanywhere.com/']
 
-MOTHERSHIP_LIMIT = os.getenv('MOTHERSHIP_LIMIT')
-SHIP_LIMIT = os.getenv('SHIP_LIMIT')
-SHIP_CREW = os.getenv('SHIP_CREW')
+MOTHERSHIP_LIMIT = 9 # os.getenv('MOTHERSHIP_LIMIT')
+SHIP_LIMIT = 5 # os.getenv('SHIP_LIMIT')
+SHIP_CREW = 3 # os.getenv('SHIP_CREW')
 
 
 INSTALLED_APPS = [
