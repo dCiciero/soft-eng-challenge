87720a8ec8ce2ecec2e91016d4e6e5eed3e536b5
Ogaga A. Ivhurie <ivhurie01@yahoo.co.uk>
2022-04-28 03:35:31 +0100

    removed systemcontrol from mship and ship

--- ship/admin.py
+++ ship/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Ship, SystemControl
+from .models import Ship
 
 # Register your models here.
 admin.site.register(Ship)
-admin.site.register(SystemControl)
--- ship/models.py
+++ ship/models.py
@@ -2,15 +2,6 @@ from django.db import models
 from mothership.models import Mothership
 
 
-# class SystemControl(models.Model):
-#     name = models.CharField(max_length=100)
-#     ship_max = models.PositiveSmallIntegerField()
-#     crew_to_add = models.PositiveSmallIntegerField(default=3)
-    
-#     def __str__(self) -> str:
-#         return f"{self.name}"
-
-
 class Ship(models.Model):
     name = models.CharField(max_length=120)
     mship = models.ForeignKey(Mothership, on_delete=models.CASCADE, related_name='ships')
